2496d6fec1e322ae49409057a13ec2c3f135540b
OscarProgra3 <Daniel.embu96nitec.edu>
2017-02-17 19:55:08 -0600

    subiendo

--- main.cpp
+++ main.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
-#include <
+#include <vector>
+#include <string>
+#include ""
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int resp=0;
+        while (resp!=4)
+        {
+                cout<< "\n Bienvenido \n 1.-Ingresar \n 2.-Listar\n 3.-Eliminar\n 4.-Salir\n  Ingrese su opcion:";
+                cin>> resp;
+        		if(resp==1)
+        		{
+        			cout<< "\n Ingresando la familia de los Stark\n";
+
+        		}
+        }
+
+
+
+	return 0;
+}
